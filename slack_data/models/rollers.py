16903a40eeb428bec95716a1cc989e42d8509c65
philipqueen <queen.philip@gmail.com>
2025-06-04 22:38:14 -0600

    Start on Rollers models, add rollers.json (mostly cleaned)

---
+++ slack_data/models/rollers.py
@@ -0,0 +1,81 @@
+from enum import Enum
+from pydantic import computed_field
+from sqlmodel import Field, Relationship, SQLModel
+
+from slack_data.utilities.currencies import Currency
+from slack_data.utilities.isa_warnings import ISAWarning
+from slack_data.utilities.materials import MetalMaterial
+
+class SliderType(Enum):
+    MovingPlates = "Moving plates"
+    Carabiner = "Carabiner"
+    LockingCarabiner = "Locking Carabiner"
+
+
+class BaseRoller(SQLModel):
+    """
+    Base class for roller version.
+    """
+    name: str = Field(index=True)
+    release_date: str | None = None
+    material: MetalMaterial
+    slider_type: SliderType
+    width: tuple[float, float] # smallest in mm, largest in mm
+    weight: float | None = None # g
+    breaking_strength: float | None = None # kN
+    isa_certified: bool = False
+    isa_warning: ISAWarning | None = None
+    colors: str | None = None # Comma separated list of colors
+    price: float | None = None 
+    currency: Currency | None = None # ISO 4217 currency code
+    description: str | None = None
+    version: str | None = None # Version indicating which batch data is from TODO: how to keep track of this?
+    notes: str | None = None
+
+class Roller(BaseRoller, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    brand_id: int = Field(foreign_key="brand.id")
+    brand: "Brand" = Relationship(back_populates="roller")
+    
+    
+    @computed_field
+    def brand_name(self) -> str:
+        """
+        Computed field to get the brand name.
+        """
+        return self.brand.name if self.brand else "Unknown"
+
+class RollerPublic(BaseRoller):
+    """
+    Model for public roller data.
+    """
+    brand_name: str
+
+    class Config:
+        orm_mode = True
+        validate_assignment = True
+        extra = "forbid"
+
+class RollerCreate(BaseRoller):
+    """
+    Model for creating a new roller entry.
+    """
+    brand_id: int
+
+    class Config:
+        exclude = ["id"]
+        validate_assignment = True
+
+class RollerUpdate(BaseRoller):
+    """
+    Model for updating an existing roller entry.
+    """
+    brand_id: int | None = None
+
+    class Config:
+        exclude = ["id"]
+        validate_assignment = True
+        extra = "forbid"
+
+
+    
